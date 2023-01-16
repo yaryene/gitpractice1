d570aa0bb1ff1662d87bedd0dd7624755e50a26b
Yared <yaredmgmt@gmail.com>
2023-01-16 18:26:33 +0300

    sample test class added

---
+++ Sampleproject2/src/com/icraft2/Test_class.java
@@ -0,0 +1,11 @@
+package com.icraft2;
+
+public class Test_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is test class");
+	}
+
+}
